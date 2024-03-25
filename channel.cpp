5f4c221c27a37469305eea5188dd6bdd69151ab7
Ilias Asrarfi <ilasrarf@e1r6p13.1337.ma>
2024-03-25 17:06:07 +0000

    add operetors in members list

--- channel.cpp
+++ channel.cpp
@@ -52,14 +52,21 @@ void check_cmd(std::map<int ,Clients>::iterator it)
     if (args[0] == "JOIN" || args[0] == "MODE")
 		pars_join_mode(args, it);
 
-	std::map<std::string, channel>::iterator pr;
-	for (pr = _channel_list.begin(); pr != _channel_list.end(); pr++)
-	{
-		std::cout << "channel name --> " << pr->first << std::endl;
-		std::cout << "operetors list --> " << pr->second._operetos_list[0] << std::endl;
-
-	}
-	std::cout << "-----------\n";
+	// std::map<std::string, channel>::iterator pr;
+	// for (pr = _channel_list.begin(); pr != _channel_list.end(); pr++)
+	// {
+	// 	std::cout << "channel name --> " << pr->first << std::endl;
+	// 	for (size_t i = 0; i < pr->second._operetos_list.size(); i++)
+	// 	{
+	// 		std::cout << "operetors list --> " << pr->second._operetos_list[i] << std::endl;
+	// 	}
+	// 	for (size_t i = 0; i < pr->second._members_list.size(); i++)
+	// 	{
+	// 		std::cout << "members list --> " << pr->second._members_list[i] << std::endl;
+	// 	}
+	// 	std::cout << std::endl;
+	// }
+	// std::cout << "-----------\n";
 	// else if (args[0] == "KICK" || args[0] == "INVITE" || args[0] == "TOPIC")
 	// 	// ft_handle_cmd(it, args);
 }
